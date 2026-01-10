1ce3d3b469c33d00afede49009ffb476471bd900
MarvinGlaser <marvin.glaser91@gmail.com>
2026-01-10 23:13:33 +0100

    base setup modules

---
+++ src/modules/audio_manager/audio_controller.rs
@@ -0,0 +1,39 @@
+use std::io::Error;
+
+
+
+struct AudioManager {
+
+}
+
+
+trait Manager {
+    fn set_colume(level: i32) -> Result<(), Error>;
+    fn play() -> Result<(), Error>;
+    fn pause() -> Result<(), Error>;
+    fn forward() -> Result<(), Error>;
+    fn backward() -> Result<(), Error>;
+}
+
+
+impl Manager for AudioManager {
+    fn set_colume(level: i32) -> Result<(), Error> {
+        todo!()
+    }
+
+    fn play() -> Result<(), Error> {
+        todo!()
+    }
+
+    fn pause() -> Result<(), Error> {
+        todo!()
+    }
+
+    fn forward() -> Result<(), Error> {
+        todo!()
+    }
+
+    fn backward() -> Result<(), Error> {
+        todo!()
+    }
+}
