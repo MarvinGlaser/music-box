aa1c348417e3017398fa24c65d841a7069228d97
MarvinGlaser <marvin.glaser91@gmail.com>
2026-01-24 11:52:39 +0100

    adjusted main a little

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,14 @@
 mod modules;
 
-use std::{error::Error/*, future::pending*/};
-//use zbus::connection;
+use std::{error::Error, future::pending};
+use zbus::{Connection, connection};
 use modules::audio_manager::audio_state_machine::{AudioState};
 use modules::audio_manager::audio_state_machine::AudioEvent::{Play, Stop, VolUp, VolDown, Forward, Back, TrackForward, TrackBack};
 
-use crate::modules::audio_manager::audio_controller::AudioManager;
-//use crate::modules::audio_manager::audio_controller::Manager;
-//use crate::modules::audio_manager::dbus_service::Greeter;
+use crate::modules::audio_manager::audio_controller::{AudioManager, Manager};
+use crate::modules::audio_manager::dbus_service::Greeter;
 
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-//fn main() {
-    //println!("Hello");
-    //let audio_manager: AudioManager = AudioManager {};
-    //audio_manager.set_volume(33).unwrap();
-    //let connection = Connection::session();
-
-    //let greeter = Greeter::new(0);
-    //let _conn = connection::Builder::session()?
-    //    .name("org.zbus.MyGreeter")?
-    //    .serve_at("/org/zbus/MyGreeter", greeter)?
-    //    .build()
-    //    .await?;
-    //pending::<()>().await;
-    
+fn test_state_machine() {
     let events = vec![
         Play,
         Play,
@@ -51,7 +34,30 @@ async fn main() -> Result<(), Box<dyn Error>> {
         state.print_state();
         state = state.on_event(event);
     }
+}
+
+async fn test_audio_manager() -> Result<(), zbus::Error> {
+    let audio_manager: AudioManager = AudioManager {};
+    audio_manager.set_volume(33).unwrap();
+    let connection = Connection::session();
+
+    let greeter = Greeter::new(0);
+    let _conn = connection::Builder::session()?
+        .name("org.zbus.MyGreeter")?
+        .serve_at("/org/zbus/MyGreeter", greeter)?
+        .build()
+        .await?;
+    pending::<()>().await;
+    Ok(())
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+//fn main() {
+    test_state_machine();
+    let res = test_audio_manager().await;
+    if let Err(err) = res { println!("Something terrible happend! Error: {err}") };
 
     Ok(())
-    
 }
